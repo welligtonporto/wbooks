53c6fec3dd93c09bb5a2a40b0bf94cbaa62b9c46
Welligton Porto <welligtonporto@gmail.com>
2017-09-14 10:52:03 -0300

    Design init

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp       	= require('gulp');
+var compass 	= require('gulp-compass');
+var cleanCSS 	= require('gulp-clean-css');
+var gcmq 	   	= require('gulp-group-css-media-queries');
+var uglify 		= require('gulp-uglify');
+var concat 		= require('gulp-concat');
+
+// Styles
+gulp.task('scss', function() {
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(compass({
+			css: 'dist/css',
+			sass: 'src/scss',
+			sourcemap: false
+		}))
+		.pipe(gcmq())
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('dist/css'));
+});
+
+// Scripts
+gulp.task('scripts', function() {
+	return gulp.src('src/js/**/*.js')
+		.pipe(uglify())
+		.pipe(concat('functions.js'))
+		.pipe(gulp.dest('dist/js'));
+});
+
+// Icons
+gulp.task('icons', function () {
+	return gulp.src('src/icons/**/*.{eot,ttf,woff,woff2,svg}')
+		.pipe(gulp.dest('dist/font'));
+});
+
+// Libs
+gulp.task('libs', function() {
+	return gulp.src([
+			'node_modules/jquery/dist/jquery.min.js',
+			// 'node_modules/bootstrap/dist/js/bootstrap.min.js',
+			'node_modules/bootstrap/dist/css/bootstrap.min.css'
+		])
+		.pipe(gulp.dest('dist/libs'));
+});
+
+// Default
+gulp.task('default', ['scss','scripts','libs','icons']);
+
+// Dev
+gulp.task('dev', ['default'], function() {
+	gulp.watch('src/scss/**/*.scss', ['scss']);
+	gulp.watch('src/js/**/*.js', ['scripts']);
+	gulp.watch('src/icons/**/*.{eot,ttf,woff,woff2,svg}', ['icons']);
+});
