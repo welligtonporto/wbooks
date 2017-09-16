b6860085056ff380137589dca0f3560b9d0e62ff
Welligton Porto <welligtonporto@gmail.com>
2017-09-16 19:39:09 -0300

    Favorite function

--- src/js/functions.js
+++ src/js/functions.js
@@ -3,7 +3,7 @@ var wBooksApp = angular.module('wBooksApp', ['ngResource','ngRoute']);
 
 // Controllers  
 // wBooksApp.controller('resultsCtrl', ['$scope', function($scope, BookService) {
-wBooksApp.controller('resultsCtrl', ['$scope', '$http', function($scope, $http) {
+wBooksApp.controller('resultsCtrl', ['$scope', '$http', 'FavoriteService', function($scope, $http, FavoriteService) {
 	$scope.searched = false;
 
     $scope.getSearch = function () {
@@ -34,13 +34,18 @@ wBooksApp.controller('resultsCtrl', ['$scope', '$http', function($scope, $http) 
 				$scope.searched = true;
 				$scope.actualSearchTerm = $scope.searchTerm;
 
+
+
 				if (index == 0) { 
 					totalItems = response.data.totalItems;
-					// console.log(totalItems);
 					$scope.bookResults = response.data.items;
+
+
 				} else {
 					$scope.bookResults = $scope.bookResults.concat(response.data.items);
 				}
+				
+				controlFavorites($scope.bookResults);
 			});
     }
 
@@ -50,7 +55,57 @@ wBooksApp.controller('resultsCtrl', ['$scope', '$http', function($scope, $http) 
 				$scope.bookData = response.data;
 			});
     }
+
+    var controlFavorites = function(items) {
+    	for (var i = 0; i < items.length; i++){
+			items[i].isLocalStorageEnable = FavoriteService.isLocalStorageEnable;
+			items[i].isFavorite = FavoriteService.isFavorite(items[i]);
+
+			itemTesting = "alok2";
+
+			items[i].markAs = function(type) {
+				thisItem = this;
+
+				switch(type) {
+					case true :
+						FavoriteService.setFav(thisItem);
+						break;
+					case false :
+						FavoriteService.deleteFav(thisItem);
+						break;
+				}
+
+				thisItem.isFavorite = type;
+			}
+    	}
+    }
 }]);
+
+// Services
+wBooksApp.service("FavoriteService",[function(){
+    this.isLocalStorageEnable = function() {
+        if(typeof (Storage) !== "undefined"){
+            return true;
+        }
+        else{
+            return false;
+        }
+    };
+
+    this.isFavorite = function(scope){
+        var fav = localStorage.getItem(scope.id);
+        return fav === "1";
+    };
+
+    this.setFav = function(item){
+        localStorage.setItem(item.id,"1");
+    };
+
+    this.deleteFav = function(item){
+        localStorage.removeItem(item.id);
+    };
+}]);
+
 
 // Filters
 wBooksApp.filter('yearFilter', function () {
